d2da04b59d92b2c0840880520468cffaa8060b1e
pour68 <pour68@outlook.com>
2025-04-16 15:12:06 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,37 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
         this.closest(".dropdown").classList.toggle("active");
     });
 });
+
+/*=============== SEARCH ===============*/
+const search = document.getElementById('search-button'),
+    searchClose = document.getElementById('search-close'),
+    searchContent = document.getElementById('search-content')
+
+if (search) {
+    search.addEventListener('click', () => {
+        searchContent.classList.add('show-search')
+    })
+}
+
+if (searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search')
+    })
+}
+
+/*=============== LOGIN ===============*/
+const login = document.getElementById('login-button'),
+    loginClose = document.getElementById('login-close'),
+    loginContent = document.getElementById('login-content')
+
+if (login) {
+    login.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
+
+if (loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login')
+    })
+}
