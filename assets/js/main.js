38b79c7adf364e090f88c565fc26fa0712e6a0bc
pour68 <pour68@outlook.com>
2025-04-18 18:05:06 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,3 @@
-const swiper = new Swiper('.special__swiper, .blog__swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-    grabCursor: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        540: {
-            centeredSlides: true,
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 4
-        },
-    }
-});
-
-const homeSwiper = new Swiper('.home__swiper', {
-    slidesPerView: 1,
-    grabCursor: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    }
-});
-
 const menu = document.querySelector(".menu"),
     closeBtn = document.querySelector(".close-menu"),
     openBtn = document.querySelector(".open-menu");
